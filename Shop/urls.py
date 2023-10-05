923bdd43e718720d0367d77d5abe8c971f994d66
Takii1 <110538879+Takii1@users.noreply.github.com>
2023-10-06 01:06:31 +0500

    updated function names

--- Shop/urls.py
+++ Shop/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index,name="Home"),
-    path('about/',views.about),
-    path('shop/',views.Shop),
-    path('contact/',views.Contact),
+    path('', views.index, name="Home"),
+    path('about/', views.about),
+    path('shop/', views.shop),
+    path('contact/', views.contact),
 ]
--- Shop/views.py
+++ Shop/views.py
@@ -2,18 +2,22 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
+
 def index(request):
     template = loader.get_template('index.html')
     return HttpResponse(template.render())
+
 
 def about(request):
     template = loader.get_template('about.html')
     return HttpResponse(template.render())
 
-def Shop(request):
+
+def shop(request):
     template = loader.get_template('shop.html')
     return HttpResponse(template.render())
 
-def Contact(request):
+
+def contact(request):
     template = loader.get_template('contact.html')
     return HttpResponse(template.render())
